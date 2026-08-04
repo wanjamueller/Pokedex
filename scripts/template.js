9f8743ba5414836615f6653f649d4b6c20f54fce
wanjamueller <me@wanjamueller.com>
2026-08-04 11:48:35 +0200

    cards visible with test names

---
+++ scripts/template.js
@@ -0,0 +1,7 @@
+function libraryTemplate(i) {
+    return /*html*/ `
+        <div class="library-cards">
+            <h3>Name: ${myPokemons[i].name}</h3>
+        </div>
+    `;
+}
---
+++ scripts/testdb.js
@@ -0,0 +1,22 @@
+const myPokemons = [
+    {
+        "name": "bulbasaur",
+        "url": "https://pokeapi.co/api/v2/pokemon/1/",
+    },
+    {
+        "name": "ivysaur",
+        "url": "https://pokeapi.co/api/v2/pokemon/2/",
+    },
+    {
+        "name": "venusaur",
+        "url": "https://pokeapi.co/api/v2/pokemon/3/",
+    },
+    {
+        "name": "charmander",
+        "url": "https://pokeapi.co/api/v2/pokemon/4/",
+    },
+    {
+        "name": "charmeleon",
+        "url": "https://pokeapi.co/api/v2/pokemon/5/",
+    },
+];
