358c191f2a4b89b8c5990a57aa78e96ad2a42610
wanjamueller <me@wanjamueller.com>
2026-08-04 14:22:16 +0200

    getting basic API data and rendering it

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 const LIBRARY_REF = document.getElementById("library-wrapper");
+const MY_PKMS = [];
 
-function renderPokemons() {
-    for (let index = 0; index < myPokemons.length; index++) {
-        LIBRARY_REF.innerHTML += libraryTemplate(index);
+// #region Render Pokemons
+
+function renderPokemons(pokemons) {
+    LIBRARY_REF.innerHTML = "";
+    for (let index = 0; index < pokemons.length; index++) {
+        const pkm = pokemons[index];
+        // LIBRARY_REF.innerHTML += libraryTemplate(pkm);
+        LIBRARY_REF.innerHTML += /*html*/ `
+        <div class="library-cards">
+            <h3>${pkm.name}</h3>
+        </div>
+    `;
     }
 }
+
+// #endregion Render Pokemons
+
+// #region get API Data
+
+async function getPkm() {
+    const Pkms = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`);
+    const PkmsFromJSON = await Pkms.json();
+    console.log(PkmsFromJSON);
+    console.log(PkmsFromJSON.results[1].name);
+
+    for (let i = 0; i < PkmsFromJSON.results.length; i++) {
+        MY_PKMS.push(PkmsFromJSON.results[i]);
+    }
+
+    renderPokemons(MY_PKMS);
+}
+
+getPkm();
+
+// #endregion get API Data
